00fed3b21a27d60a9a4079471a6cf0d0eb08bada
hcpss-vincevince1006 <vincent_dubay@hcpss.org>
2015-07-29 12:41:37 -0400

    fixed Jquery Logic
    Finally found a solution that uses .hide class and allows selection of
    multiple questions. Working on effects and the arrow feature, still has
    some bugs.

--- js/sizzle.js
+++ js/sizzle.js
@@ -1,21 +1,11 @@
-var visible = true;
-// gives all #list paragraph elements a default class of hide for accessibility purposes. This is instead of using 'display:none'
-$("#list p").addClass('hide');
-
 $("#list h3").click(function(){
-  $('#list h3').find('.fa-angle-down').removeClass('fa-angle-down').addClass('fa-angle-right');
-//slide down the link list below the h3 when clicked - but only if its closed
-    if(!$(this).next().is( visible )) {
-        $("#list p").addClass('hide');
-        $(this).next().slideUp(0,function(){
-      	
-        $(this).removeClass('hide')
-           	.slideDown('fast')
-    	  });
-
-      $(this).find('.fa-angle-right').removeClass('fa-angle-right').addClass('fa-angle-down');
-    }
-  visible = ! visible;
+      if($(this).next().hasClass('hide')) {
+          $(this).next().removeClass('hide');
+          $(this).find('.fa-angle-right').removeClass('fa-angle-right').addClass('fa-angle-down');
+      } else {
+          $(this).next().addClass('hide');
+          $('#list h3').find('.fa-angle-down').removeClass('fa-angle-down').addClass('fa-angle-right');
+      }
 });
 
 $(function() {
